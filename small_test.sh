8dcdf28a466412983045e88b17b9580713d28c07
Tom Hayden <thayden@gmail.com>
2014-11-29 21:13:31 -0600

    some small improvements & updated awk command

--- small_test.sh
+++ small_test.sh
@@ -1,6 +1,9 @@
-head -2000 ml-100k/u2.test | awk -F "\t" -f transform2.awk > /tmp/users.txt
+head -4000 ml-100k/u1.test | gawk -F "\t" -f transform2.awk > /tmp/users.txt
 #cat ml-100k/u1.test ml-100k/u2.test | awk -F "\t" -f transform2.awk > /tmp/users.txt
 wc /tmp/users.txt
-cat /tmp/users.txt | parallel --block 10K --progress --pipe awk -f blowup.awk > /tmp/results.txt
+echo "transforming and computing user-user matix"
+cat /tmp/users.txt | parallel --no-notice --block 10K --pipe gawk -f blowup.awk > /tmp/results.txt
 wc /tmp/results.txt
-cat /tmp/results.txt | sort -k1,1n -k3,3n -t, > /tmp/results2.txt
+echo "reducing to top n per user"
+cat /tmp/results.txt | sort -k1,1n -k3,3rn -t, | gawk 'BEGIN { FS = ","} { counts[$1]++; if(counts[$1]<=20) print $0 }' > /tmp/results_reduced.txt
+wc /tmp/results_reduced.txt
